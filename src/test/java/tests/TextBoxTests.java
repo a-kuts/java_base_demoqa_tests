99a8014c6a969cf8f9efbb3ea3345bdc60846b2d
anastasiya kuts <nas5772@yandex.ru>
2026-03-21 01:06:57 +0300

    moved testdata to TestData

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -2,19 +2,31 @@ package tests;
 
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static tests.testdata.TestData.*;
 
 public class TextBoxTests extends TestBase {
 
-    String userName = "ivan ivanov";
-    String userEmail = "ivan@ivanov.com";
-    String currentAddress = "true address";
-    String permanentAddress = "very true address";
+//    String userName;
+//    String userEmail;
+//    String currentAddress;
+//    String permanentAddress;
+//
+//    @BeforeEach
+//    public void setup() {
+//        userName - getRandomString(10);
+//
+//        userName = "ivan ivanov";
+//        userEmail = "ivan@ivanov.com";
+//        currentAddress = "true address";
+//        permanentAddress = "very true address";
+//   }
 
     @Test
     void successfulFillFormTest() {
---
+++ src/test/java/tests/testdata/TestData.java
@@ -0,0 +1,8 @@
+package tests.testdata;
+
+public class TestData {
+    public static String userName = "ivan ivanov";
+    public static String userEmail = "ivan@ivanov.com";
+    public static String currentAddress = "true address";
+    public static String permanentAddress = "very true address";
+}
